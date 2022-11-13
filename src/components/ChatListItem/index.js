9f72fa2e6cb83d93b7d81c2a2c4e82fdd76fc127
Priyanka <priyanka2001@gmail.com>
2022-11-13 20:11:57 +0530

    UI-3 Props

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,31 +1,32 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 
-const ChatListItem = () => {
+const ChatListItem = ({ chat }) => {
+    console.log(chat)
     return (
-        // main container
-        <View style={styles.container}>
-            <Image
-                source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg' }}
-                style={styles.image}
-            />
-
-        {/* Content Container */}
-            <View style={styles.content}>
-                {/* Row */}
-                <View style={styles.row}>
-                    <Text style={styles.name} numberOfLines={1}>
-                        Lukas
-                    </Text>
-                    <Text style={styles.subTitle}>8:30</Text>
-                </View>
-
-                <Text numberOfLines={2} style={styles.subTitle}>
-                    Hello there
-                </Text>
-            </View>
+      <View style={styles.container}>
+        {/* User Avatar */}
+        <Image source={{ uri: chat.user.image }} style={styles.image} />
+  
+        {/* content */}
+        <View style={styles.content}>
+          <View style={styles.row}>
+            <Text style={styles.name} numberOfLines={1}>
+              {chat.user.name}
+            </Text>
+  
+            <Text style={styles.subTitle}>
+              {chat.lastMessage.createdAt}
+            </Text>
+          </View>
+  
+          <Text style={styles.subTitle} numberOfLines={2}>
+            {chat.lastMessage.text}
+          </Text>
         </View>
+      </View>
     );
-};
+  };
+
 
 const styles = StyleSheet.create({
     container: {
